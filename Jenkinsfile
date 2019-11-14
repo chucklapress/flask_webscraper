e40e91fdba7725130d5b26531357adab87812fe3
chucklapress <charleslapress@gmail.com>
2019-11-14 09:28:24 -0500

    simple flask webscraper

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline {
+  agent { docker { image 'python:2.7.17' } }
+  stages {
+    stage('build') {
+      steps {
+        sh 'pip install -r requirements.txt'
+      }
+    }
+  }
+}  
---
+++ webscraper.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+
+
+source = requests.get('https://www.wherethetruck.is').text
+soup = BeautifulSoup(source,'lxml')
+big_news = soup.find('h1').text
+highlight = soup.find('h2').text
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    big_news = soup.find('h1').text
+    highlight = soup.find('h2').text
+    return render_template('index.html',**locals())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0') 
