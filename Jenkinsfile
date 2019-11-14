198315d6da37e29cfe82f4c67ab51374df39b9a4
chucklapress <charleslapress@gmail.com>
2019-11-14 12:25:24 -0500

    added tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,5 +6,20 @@ pipeline {
         sh 'pip install -r requirements.txt'
       }
     }
-  }
-}  
+    stage('Test') {
+            agent {
+                docker {
+                    image 'qnib/pytest'
+                }
+            }
+            steps {
+                sh 'py.test --verbose --junit-xml test-reports/results.xml sources/test_basic.py'
+            }
+            post {
+                always {
+                    junit 'test-reports/results.xml'
+                }
+            }
+        }
+    }
+}
---
+++ test_basic.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from webscraper import app
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = True
+        self.app = app.test_client()
+        self.assertEqual(app.debug,True)
+    def tearDown(self):
+        pass
+
+
+###############
+#### tests ####
+###############
+
+    def test_main_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
