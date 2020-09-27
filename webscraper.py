a695aa3494b8a0955e5c7d57f3097c74f9ef6797
chucklapress <charleslapress@gmail.com>
2020-09-27 15:18:51 -0400

    modified scraper app to pull luna rosa flavors

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 
-source = requests.get('http://www.wherethetruck.is').text
-soup = BeautifulSoup(source,'lxml')
-big_news = soup.find('h1').text
-highlight = soup.find('h3').text
 
+
+source = requests.get('http://lunarosa.herokuapp.com')
+soup = BeautifulSoup(source.text, 'html.parser')
+rows = soup.find_all('h2')
+for row in rows:
+    print(row.get_text())
 
 app = Flask(__name__)
 @app.route('/')
+
+
 def index():
-    big_news = soup.find('h1').text
-    highlight = soup.find('h2').text
+    now = datetime.datetime.now()
+    date = now.strftime("%m-%d-%Y")
+    rows = soup.find_all('h2')
+    for row in rows:
+        row.get_text()
     return render_template('index.html',**locals())
 
 if __name__ == '__main__':
